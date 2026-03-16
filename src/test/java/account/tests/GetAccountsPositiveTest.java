46531eff36f4cbcf0b3cbf2027faf676bc6f74b9
Andrey Kuznetsov <daniilbacardi@yandex.ru>
2026-03-16 23:58:58 +0300

    tests

--- src/test/java/account/tests/GetAccountsPositiveTest.java
+++ src/test/java/account/tests/GetAccountsPositiveTest.java
@@ -7,6 +7,8 @@ import account.User;
 import account.base.BaseGrpcTest;
 import com.google.protobuf.Int32Value;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,5 +39,75 @@ public class GetAccountsPositiveTest extends BaseGrpcTest {
             assertTrue(response.getPaging().getPageSize() >= 0);
             assertTrue(response.getPaging().getCurrentPage() >= 0);
         }
+    }
+
+    @Test
+    void getAccountsShouldIncludeInactiveAccountsWhenFlagIsTrue() {
+
+        PagingQuery paging = PagingQuery.newBuilder()
+                .setSize(Int32Value.of(50))
+                .setNumber(Int32Value.of(1))
+                .build();
+
+        GetAccountsRequest requestWithoutInactive = GetAccountsRequest.newBuilder()
+                .setWithInactive(false)
+                .setPaging(paging)
+                .build();
+
+        GetAccountsRequest requestWithInactive = GetAccountsRequest.newBuilder()
+                .setWithInactive(true)
+                .setPaging(paging)
+                .build();
+
+        GetAccountsResponse responseWithoutInactive =
+                blockingStub.getAccounts(requestWithoutInactive);
+
+        GetAccountsResponse responseWithInactive =
+                blockingStub.getAccounts(requestWithInactive);
+
+        assertNotNull(responseWithoutInactive);
+        assertNotNull(responseWithInactive);
+
+        int countWithoutInactive = responseWithoutInactive.getAccountsCount();
+        int countWithInactive = responseWithInactive.getAccountsCount();
+
+        assertTrue(
+                countWithInactive >= countWithoutInactive,
+                "withInactive=true should return same or more accounts"
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 5, 1",
+            "5, 5, 1",
+            "6, 5, 2",
+            "8, 5, 2",
+            "11, 5, 3"
+    })
+    void getAccountsShouldCalculateCurrentPageFromEntityNumber(
+            int entityNumber,
+            int pageSize,
+            int expectedCurrentPage
+    ) {
+        PagingQuery paging = PagingQuery.newBuilder()
+                .setNumber(Int32Value.of(entityNumber))
+                .setSize(Int32Value.of(pageSize))
+                .setSkip(Int32Value.of(0))
+                .build();
+
+        GetAccountsRequest request = GetAccountsRequest.newBuilder()
+                .setWithInactive(false)
+                .setPaging(paging)
+                .build();
+
+        GetAccountsResponse response = blockingStub.getAccounts(request);
+
+        assertNotNull(response);
+        assertTrue(response.hasPaging(), "Response should contain paging info");
+
+        assertEquals(pageSize, response.getPaging().getPageSize());
+        assertEquals(entityNumber, response.getPaging().getEntityNumber());
+        assertEquals(expectedCurrentPage, response.getPaging().getCurrentPage());
     }
 }
--- src/test/java/account/tests/UpdateAccountPositiveTest.java
+++ src/test/java/account/tests/UpdateAccountPositiveTest.java
@@ -1,11 +1,15 @@
 package account.tests;
 
+import account.GetCurrentAccountRequest;
+import account.GetCurrentAccountResponse;
 import account.UpdateAccountRequest;
 import account.UpdateAccountResponse;
 import account.UserDetails;
+import account.UserUpdate;
 import account.base.BaseGrpcTest;
 import account.model.AccountUpdateData;
 import account.model.TestUser;
+import com.google.protobuf.BoolValue;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,5 +38,147 @@ public class UpdateAccountPositiveTest extends BaseGrpcTest {
         assertEquals(expectedUpdate.status(), updatedUser.getStatus().getValue());
         assertEquals(expectedUpdate.name(), updatedUser.getName().getValue());
         assertEquals(expectedUpdate.location(), updatedUser.getLocation().getValue());
+    }
+
+    @Test
+    void updateAccountShouldKeepOtherFieldsUnchangedWhenOnlyRatingDisabledIsUpdated() {
+        TestUser user = userFlowSteps.registerActivateAndLogin();
+
+        GetCurrentAccountResponse beforeResponse = blockingStub.getCurrentAccount(
+                GetCurrentAccountRequest.newBuilder()
+                        .setToken(user.token())
+                        .build()
+        );
+
+        assertNotNull(beforeResponse);
+        assertTrue(beforeResponse.hasUser());
+        assertTrue(beforeResponse.getUser().hasResource());
+
+        UserDetails beforeUser = beforeResponse.getUser().getResource();
+
+        UpdateAccountRequest request = UpdateAccountRequest.newBuilder()
+                .setToken(user.token())
+                .setUserData(
+                        UserUpdate.newBuilder()
+                                .setRatingDisabled(BoolValue.of(true))
+                                .build()
+                )
+                .build();
+
+        UpdateAccountResponse response = blockingStub.updateAccount(request);
+
+        assertNotNull(response);
+        assertTrue(response.hasUser());
+        assertTrue(response.getUser().hasResource());
+
+        UserDetails updatedUser = response.getUser().getResource();
+
+        assertEquals(user.login(), updatedUser.getLogin());
+
+        assertTrue(updatedUser.hasRating(), "Updated user should contain rating");
+        assertFalse(updatedUser.getRating().getEnabled(), "rating.enabled should become false");
+
+        assertEquals(beforeUser.hasStatus(), updatedUser.hasStatus(), "Status presence should remain unchanged");
+        if (beforeUser.hasStatus()) {
+            assertEquals(beforeUser.getStatus().getValue(), updatedUser.getStatus().getValue(), "Status should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasName(), updatedUser.hasName(), "Name presence should remain unchanged");
+        if (beforeUser.hasName()) {
+            assertEquals(beforeUser.getName().getValue(), updatedUser.getName().getValue(), "Name should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasLocation(), updatedUser.hasLocation(), "Location presence should remain unchanged");
+        if (beforeUser.hasLocation()) {
+            assertEquals(beforeUser.getLocation().getValue(), updatedUser.getLocation().getValue(), "Location should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasIcq(), updatedUser.hasIcq(), "ICQ presence should remain unchanged");
+        if (beforeUser.hasIcq()) {
+            assertEquals(beforeUser.getIcq().getValue(), updatedUser.getIcq().getValue(), "ICQ should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasSkype(), updatedUser.hasSkype(), "Skype presence should remain unchanged");
+        if (beforeUser.hasSkype()) {
+            assertEquals(beforeUser.getSkype().getValue(), updatedUser.getSkype().getValue(), "Skype should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasInfo(), updatedUser.hasInfo(), "Info presence should remain unchanged");
+        if (beforeUser.hasInfo()) {
+            assertEquals(beforeUser.getInfo().getValue(), updatedUser.getInfo().getValue(), "Info should remain unchanged");
+        }
+    }
+
+    @Test
+    void updateAccountShouldKeepRatingUnchangedWhenOnlyNameIsUpdated() {
+        TestUser user = userFlowSteps.registerActivateAndLogin();
+        String expectedName = account.support.TestDataGenerator.randomName();
+
+        GetCurrentAccountResponse beforeResponse = blockingStub.getCurrentAccount(
+                GetCurrentAccountRequest.newBuilder()
+                        .setToken(user.token())
+                        .build()
+        );
+
+        assertNotNull(beforeResponse);
+        assertTrue(beforeResponse.hasUser());
+        assertTrue(beforeResponse.getUser().hasResource());
+
+        UserDetails beforeUser = beforeResponse.getUser().getResource();
+
+        UpdateAccountRequest request = UpdateAccountRequest.newBuilder()
+                .setToken(user.token())
+                .setUserData(
+                        UserUpdate.newBuilder()
+                                .setName(com.google.protobuf.StringValue.of(expectedName))
+                                .build()
+                )
+                .build();
+
+        UpdateAccountResponse response = blockingStub.updateAccount(request);
+
+        assertNotNull(response);
+        assertTrue(response.hasUser());
+        assertTrue(response.getUser().hasResource());
+
+        UserDetails updatedUser = response.getUser().getResource();
+
+        assertEquals(user.login(), updatedUser.getLogin());
+        assertTrue(updatedUser.hasName(), "Updated user should contain name");
+        assertEquals(expectedName, updatedUser.getName().getValue(), "Name should be updated");
+
+        assertEquals(beforeUser.hasRating(), updatedUser.hasRating(), "Rating presence should remain unchanged");
+        if (beforeUser.hasRating()) {
+            assertEquals(
+                    beforeUser.getRating().getEnabled(),
+                    updatedUser.getRating().getEnabled(),
+                    "Rating enabled flag should remain unchanged"
+            );
+        }
+
+        assertEquals(beforeUser.hasStatus(), updatedUser.hasStatus(), "Status presence should remain unchanged");
+        if (beforeUser.hasStatus()) {
+            assertEquals(beforeUser.getStatus().getValue(), updatedUser.getStatus().getValue(), "Status should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasLocation(), updatedUser.hasLocation(), "Location presence should remain unchanged");
+        if (beforeUser.hasLocation()) {
+            assertEquals(beforeUser.getLocation().getValue(), updatedUser.getLocation().getValue(), "Location should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasIcq(), updatedUser.hasIcq(), "ICQ presence should remain unchanged");
+        if (beforeUser.hasIcq()) {
+            assertEquals(beforeUser.getIcq().getValue(), updatedUser.getIcq().getValue(), "ICQ should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasSkype(), updatedUser.hasSkype(), "Skype presence should remain unchanged");
+        if (beforeUser.hasSkype()) {
+            assertEquals(beforeUser.getSkype().getValue(), updatedUser.getSkype().getValue(), "Skype should remain unchanged");
+        }
+
+        assertEquals(beforeUser.hasInfo(), updatedUser.hasInfo(), "Info presence should remain unchanged");
+        if (beforeUser.hasInfo()) {
+            assertEquals(beforeUser.getInfo().getValue(), updatedUser.getInfo().getValue(), "Info should remain unchanged");
+        }
     }
 }
